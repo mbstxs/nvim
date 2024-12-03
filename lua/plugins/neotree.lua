1a5b7a6d592815e0be339a51e542beb063d60f89
Marcelo Bastos <xxatkinsxx@gmail.com>
2024-12-04 00:35:00 +0100

    update neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,40 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				width = 25,
+			},
+			filesysytem = {
+				follow_current_file = true,
+				hijack_netrw_behavior = "open_default",
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_by_name = {
+						".git",
+						"DS_Store",
+					},
+					always_show = {
+						".env",
+					},
+				},
+			},
+			close_if_last_window = true,
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>", { noremap = true, silent = true })
+	end,
+}
